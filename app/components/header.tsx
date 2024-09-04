9d4da1768b913d5a6f570a0dfcc7eb790db6f7f5
Geoff Corvera <geoffcorvera@gmail.com>
2024-09-04 01:50:16 -0700

    header height full screen

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,26 +1,34 @@
+import LinkRow from "./link-row";
+
 const sections = ["about", "experience", "projects", "contact"];
 
 export default function Header() {
   return (
-    <header className="lg:flex lg:top-0 flex-col justify-between lg:w-1/2 lg:max-w-lg">
-      <h1 className="text-5xl font-bold">Geoffrey Corvera</h1>
-      <span className="text-xl font-semibold my-5 opacity-70">
-        Quality Assurance Engineer
-      </span>
-      <p className="text-lg opacity-55">
-        I help teams develop accessible, user-friendly, and reliable web applications.
-      </p>
-      <nav aria-label="section links">
-        <ul className="mt-10 w-max">
-          {sections.map((section) => {
-            return (
+    <header className="lg:flex lg:top-0 lg:flex-col justify-between lg:w-1/2 lg:max-w-lg lg:min-h-screen">
+      <div>
+        <h1 className="text-5xl font-bold">Geoffrey Corvera</h1>
+        <p className="text-xl font-semibold py-5 opacity-70">
+          Quality Assurance Engineer
+        </p>
+        <p className="text-lg opacity-55">
+          I help teams develop accessible, user-friendly, and reliable web
+          applications.
+        </p>
+        <nav aria-label="section links">
+          <ul className="mt-10 w-max">
+            {sections.map((section) => {
+              return (
                 <li key={section}>
-                    <a href={`#${section.toLowerCase()}`} className="py-2 flex">{section}</a>
+                  <a href={`#${section.toLowerCase()}`} className="py-2 flex">
+                    {section}
+                  </a>
                 </li>
-            )
-          })}
-        </ul>
-      </nav>
+              );
+            })}
+          </ul>
+        </nav>
+      </div>
+      <LinkRow />
     </header>
   );
 }
