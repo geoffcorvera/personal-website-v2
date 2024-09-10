af80325827e64c9d9949700c2d76b5fec85f573d
Geoff Corvera <geoffcorvera@gmail.com>
2024-09-10 15:47:40 -0700

    #1 add top-bottom margins to Header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,21 +5,20 @@ const sections = ["about", "experience", "projects", "contact"];
 export default function Header() {
   return (
     <header className="lg:flex lg:top-0 lg:flex-col justify-between lg:w-1/2 lg:max-w-lg lg:min-h-screen">
-      <div>
+      <div className="mt-16">
         <h1 className="text-5xl font-bold">Geoffrey Corvera</h1>
         <p className="text-xl font-semibold py-5 opacity-70">
-          Quality Assurance Engineer
+          QA Engineer
         </p>
         <p className="text-lg opacity-55">
-          I help teams develop accessible, user-friendly, and reliable web
-          applications.
+          I help teams deliver reliable, accessible, and user-friendly.
         </p>
         <nav aria-label="section links">
           <ul className="mt-10 w-max">
             {sections.map((section) => {
               return (
                 <li key={section}>
-                  <a href={`#${section.toLowerCase()}`} className="py-2 flex">
+                  <a href={`#${section.toLowerCase()}`} className="py-2 flex uppercase">
                     {section}
                   </a>
                 </li>
@@ -28,7 +27,9 @@ export default function Header() {
           </ul>
         </nav>
       </div>
-      <LinkRow />
+      <div className="mb-16">
+        <LinkRow />
+      </div>
     </header>
   );
 }
