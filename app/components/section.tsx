0c9b3ae1ce6d78e9c30809850329b4a945039732
Geoff Corvera <geoffcorvera@gmail.com>
2024-09-10 23:49:16 -0700

    #7 section component

---
+++ app/components/section.tsx
@@ -0,0 +1,16 @@
+export default function Section({
+  children,
+  name,
+  showSectionTitle = true,
+}: {
+  name: string;
+  children: React.ReactNode;
+  showSectionTitle?: boolean;
+}) {
+  return (
+    <section className="flex flex-col pt-4" id={name.toLowerCase()}>
+      <h3 className={showSectionTitle ? "text-2xl font-bold" : "hidden"}>{name}</h3>
+      {children}
+    </section>
+  );
+}
