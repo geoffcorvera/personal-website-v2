998aa695d306a109cfbcc4913b4bff9fa039afc0
Geoff Corvera <geoffcorvera@gmail.com>
2024-09-10 21:55:03 -0700

    #2 two-column layout with sticky header

--- app/page.tsx
+++ app/page.tsx
@@ -1,102 +1,67 @@
 import Header from "./components/header";
 
-function Links() {
-  return (
-    <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-      <a
-        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h2 className="mb-3 text-2xl font-semibold">
-          Docs{" "}
-          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
-          </span>
-        </h2>
-        <p className="m-0 max-w-[30ch] text-sm opacity-50">
-          Find in-depth information about Next.js features and API.
-        </p>
-      </a>
-
-      <a
-        href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h2 className="mb-3 text-2xl font-semibold">
-          Learn{" "}
-          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
-          </span>
-        </h2>
-        <p className="m-0 max-w-[30ch] text-sm opacity-50">
-          Learn about Next.js in an interactive course with&nbsp;quizzes!
-        </p>
-      </a>
-
-      <a
-        href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h2 className="mb-3 text-2xl font-semibold">
-          Templates{" "}
-          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
-          </span>
-        </h2>
-        <p className="m-0 max-w-[30ch] text-sm opacity-50">
-          Explore starter templates for Next.js.
-        </p>
-      </a>
-
-      <a
-        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h2 className="mb-3 text-2xl font-semibold">
-          Deploy{" "}
-          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
-          </span>
-        </h2>
-        <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-          Instantly deploy your Next.js site to a shareable URL with Vercel.
-        </p>
-      </a>
-    </div>
-  );
-}
-
-function MyGridLayout() {
-  return (
-    <div className="grid lg:grid-cols-3 lg:w-full lg:max-w-5xl text-left">
-      <p className="max-w-[30ch] text-sm opacity-50">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
-        maiores eaque quia.
-      </p>
-      <p className="max-w-[30ch] text-sm opacity-50">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
-        maiores eaque quia.
-      </p>
-      <p className="max-w-[30ch] text-sm opacity-50">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
-        maiores eaque quia.
-      </p>
-    </div>
-  );
-}
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-0">
-      <Header />
-    </main>
+    <div className="mx-auto min-h-screen max-w-screen-xl bg-teal-200">
+      <div className="flex justify-between gap-4">
+        <Header />
+        <main className="h-[200vh] w-1/2 flex flex-col gap-16 md:gap-24 lg:gap-36">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor vel,
+            rerum facere voluptatibus laboriosam neque voluptate ullam
+            necessitatibus nemo qui eius inventore dolore nihil doloribus, enim
+            accusantium nobis, blanditiis repellat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor vel,
+            rerum facere voluptatibus laboriosam neque voluptate ullam
+            necessitatibus nemo qui eius inventore dolore nihil doloribus, enim
+            accusantium nobis, blanditiis repellat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor vel,
+            rerum facere voluptatibus laboriosam neque voluptate ullam
+            necessitatibus nemo qui eius inventore dolore nihil doloribus, enim
+            accusantium nobis, blanditiis repellat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor vel,
+            rerum facere voluptatibus laboriosam neque voluptate ullam
+            necessitatibus nemo qui eius inventore dolore nihil doloribus, enim
+            accusantium nobis, blanditiis repellat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor vel,
+            rerum facere voluptatibus laboriosam neque voluptate ullam
+            necessitatibus nemo qui eius inventore dolore nihil doloribus, enim
+            accusantium nobis, blanditiis repellat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor vel,
+            rerum facere voluptatibus laboriosam neque voluptate ullam
+            necessitatibus nemo qui eius inventore dolore nihil doloribus, enim
+            accusantium nobis, blanditiis repellat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor vel,
+            rerum facere voluptatibus laboriosam neque voluptate ullam
+            necessitatibus nemo qui eius inventore dolore nihil doloribus, enim
+            accusantium nobis, blanditiis repellat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor vel,
+            rerum facere voluptatibus laboriosam neque voluptate ullam
+            necessitatibus nemo qui eius inventore dolore nihil doloribus, enim
+            accusantium nobis, blanditiis repellat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor vel,
+            rerum facere voluptatibus laboriosam neque voluptate ullam
+            necessitatibus nemo qui eius inventore dolore nihil doloribus, enim
+            accusantium nobis, blanditiis repellat.
+          </p>
+        </main>
+      </div>
+    </div>
   );
 }
