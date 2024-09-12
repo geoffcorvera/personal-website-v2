0ea772404d0d3d4604f0419f8b5d1e114b3058c4
Geoff Corvera <geoffcorvera@gmail.com>
2024-09-11 21:51:48 -0700

    #3 track active section

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,20 +5,18 @@ export default function Header({ activeSection, sections }: { activeSection: str
     <header className="lg:flex lg:sticky lg:top-0 lg:flex-col justify-between w-1/2 lg:max-w-xl h-screen lg:min-h-screen">
       <div className="lg:py-36">
         <h1 className="text-5xl font-serif font-semibold">Geoffrey Corvera</h1>
-        <p className="text-xl font-serif py-5 opacity-70">
-          QA Engineer
-        </p>
+        <p className="text-xl font-serif py-5 opacity-70">QA Engineer</p>
         <p className="text-lg opacity-55">
           I help teams deliver reliable, accessible, and user-friendly.
         </p>
         <nav aria-label="section links">
-          <ul className="mt-10 w-max">
+          <ul className="mt-10 w-max flex flex-col gap-4">
             {sections.map((section) => {
               return (
                 <li key={section}>
                   <a
                     href={`#${section.toLowerCase()}`}
-                    className="py-2 flex uppercase"
+                    className={`${section == activeSection && "active "}py-2 uppercase`}
                   >
                     {section}
                   </a>
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,37 @@
+"use client";
+
 import Header from "./components/header";
 import Job from "./components/job";
 import Section from "./components/section";
 import { jobs } from "./fixtures/jobDetails";
+import { useEffect, useState } from "react";
 
 const SECTIONS = ["about", "experience", "projects", "contact"];
 const aboutCopy = `I help teams deploy reliable and usable software and interfaces through a combination of strategic test planning, automation, accessibility auditing and user experience research. The ways technology enables, or gets in our way, fascinates me. It’s a privilege to play a role in keeping software a positive aspect of people’s lives.`;
 
 export default function Home() {
+  const [scrollY, setScrollY] = useState(0);
+  const [active, setActive] = useState<string>(SECTIONS[0]);
+
+  const scrollYInSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    const top = section?.offsetTop;
+    const height = section?.offsetHeight;
+    return top && height && scrollY >= top && scrollY <= top + height;
+  };
+
+  useEffect(() => {
+    document.addEventListener("scroll", () => setScrollY(window.scrollY));
+  }, []);
+
+  useEffect(() => {
+    setActive(SECTIONS.find(scrollYInSection) ?? SECTIONS[0]);
+  }, [scrollY]);
+
   return (
     <div className="mx-auto min-h-screen max-w-screen-xl">
       <div className="flex justify-between gap-4">
-        <Header sections={SECTIONS} />
+        <Header activeSection={active} sections={SECTIONS} />
         <main className="min-h-fit w-1/2 flex flex-col gap-28 lg:pt-36 lg:pb-36">
           <Section name="About" showSectionTitle={false}>
             <p className="text-lg font-light">{aboutCopy}</p>
