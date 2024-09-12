10578c1aba8505536f912efe208bc895631a9f5f
Geoff Corvera <geoffcorvera@gmail.com>
2024-09-11 20:03:18 -0700

    #8 new kansas font

--- app/components/section.tsx
+++ app/components/section.tsx
@@ -9,7 +9,13 @@ export default function Section({
 }) {
   return (
     <section className="flex flex-col pt-4" id={name.toLowerCase()}>
-      <h3 className={showSectionTitle ? "text-2xl font-bold" : "hidden"}>{name}</h3>
+      <h3
+        className={
+          showSectionTitle ? "text-2xl font-serif" : "hidden"
+        }
+      >
+        {name}
+      </h3>
       {children}
     </section>
   );
