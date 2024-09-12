8708a8c214e1f1f49e85bfb97e9bb16cab67e181
Geoff Corvera <geoffcorvera@gmail.com>
2024-09-11 20:18:35 -0700

    #21 smooth scrolling & padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className + " bg-soil text-white"}>
+    <html lang="en" className="scroll-smooth scroll-pt-32">
+      <body className={inter.className + " bg-soil text-white scroll-smooth"}>
         {children}
       </body>
     </html>
