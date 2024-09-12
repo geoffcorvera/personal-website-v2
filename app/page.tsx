c4bbc22e2c6fffd9e7d90a60df672e89dd78f62f
Geoff Corvera <geoffcorvera@gmail.com>
2024-09-12 10:36:51 -0700

    Merge branch 'test'

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Section from "./components/section";
 import { jobs } from "./fixtures/jobDetails";
 import { useEffect, useState } from "react";
 
+const REM = 16;
 const SECTIONS = ["about", "experience", "projects", "contact"];
 const aboutCopy = `I help teams deploy reliable and usable software and interfaces through a combination of strategic test planning, automation, accessibility auditing and user experience research. The ways technology enables, or gets in our way, fascinates me. It’s a privilege to play a role in keeping software a positive aspect of people’s lives.`;
 
@@ -13,11 +14,9 @@ export default function Home() {
   const [scrollY, setScrollY] = useState(0);
   const [active, setActive] = useState<string>(SECTIONS[0]);
 
-  const scrollYInSection = (sectionId: string) => {
-    const section = document.getElementById(sectionId);
-    const top = section?.offsetTop;
-    const height = section?.offsetHeight;
-    return top && height && scrollY >= top && scrollY <= top + height;
+  const scrollYInSection = (offset = 0) =>(sectionId: string) => {
+    const top = document.getElementById(sectionId)?.offsetTop;
+    return top && scrollY >= top - offset;
   };
 
   useEffect(() => {
@@ -25,14 +24,14 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
-    setActive(SECTIONS.find(scrollYInSection) ?? SECTIONS[0]);
+    setActive(SECTIONS.filter(scrollYInSection(9*REM)).pop() ?? SECTIONS[0]);
   }, [scrollY]);
 
   return (
     <div className="mx-auto min-h-screen max-w-screen-xl">
       <div className="flex justify-between gap-4">
         <Header activeSection={active} sections={SECTIONS} />
-        <main className="min-h-fit w-1/2 flex flex-col gap-28 lg:pt-36 lg:pb-36">
+        <main className="min-h-fit w-1/2 flex flex-col gap-32 lg:pt-36 lg:pb-36">
           <Section name="About" showSectionTitle={false}>
             <p className="text-lg font-light">{aboutCopy}</p>
           </Section>
